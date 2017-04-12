003f9c02b3627b8f76872fefa5e57ee395ee9fa5
k <brood915@gmail.com>
2017-04-12 16:04:47 -0400

    rewrote for webpack 2 with more plugins.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import routes from './config/routes';
+import MainContainer from './containers/MainContainer';
 
 ReactDOM.render(
-    routes,
+    <MainContainer />,
     document.getElementById('app')  
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,59 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: __dirname + '/app/index.html',
-    filename: 'index.html',
-    inject: 'body'
-});
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+//     template: __dirname + '/app/index.html',
+//     filename: 'index.html',
+//     inject: 'body'
+// });
 
+
+// module.exports = {
+//     entry: [
+//         './app/index.js'
+//     ],
+//     output: {
+//         path: __dirname + '/dist',
+//         filename: 'index_bundle.js'
+//     },
+//     module: {
+//         loaders: [
+//             {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" } 
+//         ]
+//     },
+//     plugins: [HtmlWebpackPluginConfig],
+//     watch:true
+// }
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: [
-        './app/index.js'
-    ],
-    output: {
-        path: __dirname + '/dist',
-        filename: 'index_bundle.js'
-    },
-    module: {
-        loaders: [
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" } 
-        ]
-    },
-    plugins: [HtmlWebpackPluginConfig]
-}
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    index: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath: '/assets/', 
+  },
+  module: {
+    rules: [
+      {
+      test: /\.(js|jsx)$/, 
+      include: [
+          path.resolve(__dirname, "src")
+        ],
+      use: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+  contentBase: path.join(__dirname, "dist"),
+  compress: true,
+  port: 1337
+},
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
+};
